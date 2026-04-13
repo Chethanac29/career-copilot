ecd917ec84b905078a09af50f72f41cebd42ced8
unknown <chethangirish2909@gmail.com>
2026-04-13 23:35:59 +0530

    edit: used the routes inside the main file to run the application with the resume generation feature

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-
+from app.routes import resume
+from app.services.github_service import GitHubService
 app =FastAPI()
 
+# register the routes
 
+app.include_router(resume.router)
 @app.get("/")
 def home():
     return {"message": "Career Copilot is Running!"}
